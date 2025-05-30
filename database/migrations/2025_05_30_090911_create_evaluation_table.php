0a72ab45a8ee6d520fe97305a840d1101d58da0f
Ousseynou210 <oussfayaa@gmail.com>
2025-05-30 09:51:29 +0000

    creation des migrations

---
+++ database/migrations/2025_05_30_090911_create_evaluation_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function PHPSTORM_META\type;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('evaluation', function (Blueprint $table) {
+            $table->id();
+            $table->string('libelle');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evaluation');
+    }
+};
---
+++ database/migrations/2025_05_30_091411_create_note_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('note', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('etudiant_id');
+            $table->unsignedBigInteger('evaluation_id');
+            $table->double('note');
+            $table->timestamps();
+
+            
+            $table->foreign('etudiant_id')->references('id')->on('etudiants')->onDelete('cascade');
+            $table->foreign('evaluation_id')->references('id')->on('evaluation')->onDelete('cascade');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('note');
+    }
+};
